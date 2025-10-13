e8282ceac90512563823b3733c7e47f80a1d552e
KlimchukKS <klinchuk1996@yandex.ru>
2025-10-13 20:48:03 +0300

    Create main.cpp

---
+++ 1470. Shuffle the Array/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> result;
+        result.reserve(nums.size());
+
+        for (auto i = 0, j = n; j < nums.size(); ++i, ++j) {
+            result.push_back(nums[i]);
+            result.push_back(nums[j]);
+        }
+
+        return result;
+    }
+};
